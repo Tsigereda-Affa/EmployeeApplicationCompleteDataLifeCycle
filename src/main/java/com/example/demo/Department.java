014b5d4fe969b6aca5e038394b047285360256f1
tsigereda-affa <affa16.a@gmail.com>
2018-08-20 14:57:12 -0400

    Heroku commit

--- src/main/java/com/example/demo/Department.java
+++ src/main/java/com/example/demo/Department.java
@@ -6,6 +6,7 @@ import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Entity
+@Table(name="DEPARTMENT_DATA")
 public class Department {
     @Id
     @GeneratedValue(strategy  = GenerationType.AUTO)
--- src/main/java/com/example/demo/Employee.java
+++ src/main/java/com/example/demo/Employee.java
@@ -6,6 +6,8 @@ import javax.validation.constraints.Size;
 
 
     @Entity
+    //THIS IS ADDED TO MAKE THE HEROKU WORK
+    @Table(name="EMPLOYEE_DATA")
     public class Employee {
         @Id
         @GeneratedValue(strategy = GenerationType.AUTO)
